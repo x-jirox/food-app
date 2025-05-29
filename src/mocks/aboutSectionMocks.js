da704df4c60e3951b3765946bf964cdb895a095d
x-jirox <correalenin045@gmail.com>
2025-05-29 14:13:58 -0500

    add about section and section2 to Home page

---
+++ src/mocks/aboutSectionMocks.js
@@ -0,0 +1,22 @@
+// src/mocks/aboutSectionMocks.js 
+import Pizza from '../assets/about/pizza.png'
+import Salad from '../assets/about/salad.png'
+import Delivery from '../assets/about/delivery-bike.png'
+
+export const aboutSectionMocks = [
+  {
+    image: Pizza,
+    title: "Original",
+    paragraph: `Porta semper lacus cursus, feugiat primis ultrice a ligula risus auctor an tempus feugiat dolor lacinia cubilia curae integer orci congue and metus integer primis in integer metus`,
+  },
+  {
+    image: Salad,
+    title: "Qualty Foods",
+    paragraph: `Porta semper lacus cursus, feugiat primis ultrice a ligula risus auctor an tempus feugiat dolor lacinia cubilia curae integer orci congue and metus integer primis in integer metus`,
+  },
+  {
+    image: Delivery,
+    title: "Fastest Delivery",
+    paragraph: `Porta semper lacus cursus, feugiat primis ultrice a ligula risus auctor an tempus feugiat dolor lacinia cubilia curae integer orci congue and metus integer primis in integer metus`,
+  },
+]
