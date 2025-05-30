d1be4221cdb5a1194d4dc5fc238d51323670169f
x-jirox <correalenin045@gmail.com>
2025-05-30 15:59:25 -0500

    add new Section4 component to Home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,6 +4,7 @@ import '../../styles/HomeStyle.css'
 import Section1 from './Section1'
 import Section2 from './Section2'
 import Section3 from './Section3'
+import Section4 from './Section4'
 
 function Home() {
   return (
@@ -15,6 +16,8 @@ function Home() {
       <Section2 />
       {/* Section 3 menu */}
       <Section3 />
+      {/* Section 4 promotion */}
+      <Section4 />
     </Layout>
     </>
   )
